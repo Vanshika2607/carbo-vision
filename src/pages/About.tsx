1d9990d0c879407ba8ca90a82bab51421c31a8dc
Vanshika <vanshika5603sjps@gmail.com>
2025-08-27 17:07:02 +0530

    added

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -149,7 +149,7 @@ const About = () => {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
             {team.map((member, index) => (
               <div
                 key={index}
