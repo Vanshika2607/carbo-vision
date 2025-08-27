8bf7234a3536c526a9a3ab4f28630db27a6e3714
Vanshika <vanshika5603sjps@gmail.com>
2025-08-27 16:54:05 +0530

    updated

--- src/data/products.ts
+++ src/data/products.ts
@@ -93,7 +93,7 @@ export const products: Product[] = [
     images: [
       'https://i.postimg.cc/hjHMpFD8/iot-curtains.jpg'
     ],
-    description: 'Want your office/home to be automated through voice or remote .. Try these',
+    description: 'Want your office/home to be automated through voice ! Try these',
     features: [
        'Mobile App Control',
 'Voice Assistant Integration',
